2bbfa9406be7c0bef72c19083de25aa70b2eaf3a
LongNguyen-51101909 <longnguyenqu@gmail.com>
2016-10-04 08:34:33 +0700

    back to top

--- js/standard.object.js
+++ js/standard.object.js
@@ -28,7 +28,7 @@ $(document).ready(function(){
     });
     //Check to see if the window is top if not then display button
     $(window).scroll(function(){
-        if ($(this).scrollTop() > 1500) {
+        if ($(this).scrollTop() > 600) {
             $('.scrollToTop').fadeIn();
         } else {
             $('.scrollToTop').fadeOut();
